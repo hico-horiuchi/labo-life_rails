318d74d639d3978170ad76b93d95462ac977c538
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-04-24 17:43:01 +0900

    User seedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# User
+User.destroy_all
+User.create(name: '教員', student_no: 'teacher', email: 'teacher@sss.jp', password: 'password', password_confirmation: 'password', level: 2)
+User.create(name: '助手', student_no: 'assistant', email: 'assistant@sss.jp', password: 'password', password_confirmation: 'password', level: 1)
+User.create(name: '学生', student_no: 'student', email: 'student@sss.jp', password: 'password', password_confirmation: 'password', level: 0)
