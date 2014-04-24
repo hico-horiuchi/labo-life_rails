0d13cd90341cc450ac2dde660f17f24bc0a2effb
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-04-24 17:47:17 +0900

    User seedのメールアドレスを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 # User
 User.destroy_all
-User.create(name: '教員', student_no: 'teacher', email: 'teacher@sss.jp', password: 'password', password_confirmation: 'password', level: 2)
-User.create(name: '助手', student_no: 'assistant', email: 'assistant@sss.jp', password: 'password', password_confirmation: 'password', level: 1)
-User.create(name: '学生', student_no: 'student', email: 'student@sss.jp', password: 'password', password_confirmation: 'password', level: 0)
+User.create(name: '教員', student_no: 'teacher', email: 'teacher@labolife.jp', password: 'password', password_confirmation: 'password', level: 2)
+User.create(name: '助手', student_no: 'assistant', email: 'assistant@labolife.jp', password: 'password', password_confirmation: 'password', level: 1)
+User.create(name: '学生', student_no: 'student', email: 'student@labolife.jp', password: 'password', password_confirmation: 'password', level: 0)
