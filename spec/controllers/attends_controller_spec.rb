243caa3a9b79bdc3b6f84f55d8c9dfcea7746dcd
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-04-30 17:58:05 +0900

    Attend new,edit,updateのテストを作成

--- spec/controllers/attends_controller_spec.rb
+++ spec/controllers/attends_controller_spec.rb
@@ -1,21 +1,66 @@
 require 'spec_helper'
 
 describe AttendsController do
-  describe '#create' do
+  describe '#new' do
     before do
       user = FactoryGirl.create(:assistant)
       session[:user_id] = user.id
-
       seminor_params = FactoryGirl.attributes_for(:seminor)
       seminor_params[:chairman_user_id] = user.id
       seminor_params[:created_user_id] = user.id
       seminor = Seminor.create(seminor_params)
+      xhr :get, :new, seminor_id: seminor.id
+    end
+    it { expect(response).to be_success }
+  end
 
+  describe '#create' do
+    before do
+      user = FactoryGirl.create(:assistant)
+      session[:user_id] = user.id
+      seminor_params = FactoryGirl.attributes_for(:seminor)
+      seminor_params[:chairman_user_id] = user.id
+      seminor_params[:created_user_id] = user.id
+      seminor = Seminor.create(seminor_params)
       attend_params = {}
       attend_params[:user_id] = user.id
       attend_params[:seminor_id] = seminor.id
-
       xhr :post, :create, attend: attend_params
+    end
+    it { expect(response).to be_success }
+    it { expect(assigns[:result]).to be_true }
+  end
+
+  describe '#edit' do
+    before do
+      user = FactoryGirl.create(:assistant)
+      session[:user_id] = user.id
+      seminor_params = FactoryGirl.attributes_for(:seminor)
+      seminor_params[:chairman_user_id] = user.id
+      seminor_params[:created_user_id] = user.id
+      seminor = Seminor.create(seminor_params)
+      attend_params = {}
+      attend_params[:user_id] = user.id
+      attend_params[:seminor_id] = seminor.id
+      attend = Attend.create(attend_params)
+      xhr :get, :edit, id: attend.id
+    end
+    it { expect(response).to be_success }
+  end
+
+  describe '#update' do
+    before do
+      user = FactoryGirl.create(:assistant)
+      session[:user_id] = user.id
+      seminor_params = FactoryGirl.attributes_for(:seminor)
+      seminor_params[:chairman_user_id] = user.id
+      seminor_params[:created_user_id] = user.id
+      seminor = Seminor.create(seminor_params)
+      attend_params = {}
+      attend_params[:user_id] = user.id
+      attend_params[:seminor_id] = seminor.id
+      attend = Attend.create(attend_params)
+      xhr :patch, :update, id: attend.id, attend: attend_params
     end
     it { expect(response).to be_success }
     it { expect(assigns[:result]).to be_true }
@@ -25,12 +70,10 @@ describe AttendsController do
     before do
       user = FactoryGirl.create(:assistant)
       session[:user_id] = user.id
-
       seminor_params = FactoryGirl.attributes_for(:seminor)
       seminor_params[:chairman_user_id] = user.id
       seminor_params[:created_user_id] = user.id
       seminor = Seminor.create(seminor_params)
-
       attend = Attend.create(user_id: user.id, seminor_id: seminor.id)
       xhr :delete, :destroy, id: attend.id
     end
